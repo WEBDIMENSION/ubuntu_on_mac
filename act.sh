04ec3204ec3809c72ae84025a640d0c695214018
webdimension <office@webdimension.jp>
2020-09-22 08:27:04 +0000

    Fix actions

--- act.sh
+++ act.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-docker rm -f dev
-docker rmi  dev
+docker rm -f ubuntu18.04
+docker rmi  ubuntu18.04
+docker rm -f ubuntu20.04
+docker rmi  ubuntu20.04
 docker rm -f act-tests-build
 act push
